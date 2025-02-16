9793b8b1396202e62c52aa33a357136227908b22
YlmzCmlttn <ylmzcmlttn@gmail.com>
2025-02-16 20:39:04 +0300

    Phillips Spectrum Height Map calculation

--- Assets/Scripts/PhillipsSpectrum.cs
+++ Assets/Scripts/PhillipsSpectrum.cs
@@ -7,8 +7,13 @@ public class PhillipsSpectrum : MonoBehaviour
 {
     // Start is called before the first frame update
     public ComputeShader fftComputeShader;
-    public GameObject debugQuad;
+    public GameObject initialSpectrumDebugQuad;
+    public GameObject updatedSpectrumDebugQuad;
+    public GameObject heightMapDebugQuad;
     private RenderTexture initialSpectrumTexture;
+    private RenderTexture updatedSpectrumTexture;
+    private RenderTexture heightMapTexture;
+
 
     public float _Wind_DirX = -1.0f;
     public float _Wind_DirY = 1.0f;
@@ -16,8 +21,14 @@ public class PhillipsSpectrum : MonoBehaviour
     public float _A = 20;
     public int _Resolution = 512;
     public int _PhysicalDomainLength = 1024;
+    public bool UpdateInitialSpectrum = false;
 
     private int threadGroupsX, threadGroupsY;
+    private int CalculateInitialSpectrumKernelIndex;
+    private int UpdateSpectrumKernelIndex;
+    private int HeightMapKernelIndex;
+
+
 
     private void OnEnable() {
         threadGroupsX = Mathf.CeilToInt(_Resolution / 8.0f);
@@ -27,33 +38,66 @@ public class PhillipsSpectrum : MonoBehaviour
         initialSpectrumTexture.enableRandomWrite = true;
         initialSpectrumTexture.Create();
 
+        updatedSpectrumTexture = new RenderTexture(_Resolution, _Resolution, 0, RenderTextureFormat.RGHalf, RenderTextureReadWrite.Linear);
+        updatedSpectrumTexture.enableRandomWrite = true;
+        updatedSpectrumTexture.Create();
+
+        heightMapTexture = new RenderTexture(_Resolution, _Resolution, 0, RenderTextureFormat.RHalf, RenderTextureReadWrite.Linear);
+        heightMapTexture.enableRandomWrite = true;
+        heightMapTexture.Create();
+
+        
+
+        CalculateInitialSpectrumKernelIndex = fftComputeShader.FindKernel("CS_CalculateInitialSpectrum");
+        UpdateSpectrumKernelIndex = fftComputeShader.FindKernel("CS_UpdateSpectrum");
+        HeightMapKernelIndex = fftComputeShader.FindKernel("CS_ComputeHeightMap");
+
+
+        UpdateInitalSpectrumTexture();
+    }
+
+    void UpdateInitalSpectrumTexture() {
         fftComputeShader.SetFloat("_Wind_DirX", _Wind_DirX);
         fftComputeShader.SetFloat("_Wind_DirY", _Wind_DirY);
         fftComputeShader.SetFloat("_WindSpeed", _WindSpeed);
         fftComputeShader.SetFloat("_A", _A);
         fftComputeShader.SetInt("_Resolution", _Resolution);
         fftComputeShader.SetInt("_PhysicalDomainLength", _PhysicalDomainLength);
-        fftComputeShader.SetTexture(0, "_InitialSpectrumTexture", initialSpectrumTexture);
-        fftComputeShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        
 
-        Material debugMat = debugQuad.GetComponent<Renderer>().material;
+        fftComputeShader.SetTexture(CalculateInitialSpectrumKernelIndex, "_InitialSpectrumTexture", initialSpectrumTexture);
+        fftComputeShader.Dispatch(CalculateInitialSpectrumKernelIndex, threadGroupsX, threadGroupsY, 1);
+
+        Material debugMat = initialSpectrumDebugQuad.GetComponent<Renderer>().material;
         debugMat.SetTexture("_DebugTexture", initialSpectrumTexture);
+    }
+
+    void UpdateSpectrumTexture() {
+        fftComputeShader.SetInt("_Resolution", _Resolution);
+        fftComputeShader.SetInt("_PhysicalDomainLength", _PhysicalDomainLength);
+        fftComputeShader.SetFloat("_FrameTime", Time.time);
+        fftComputeShader.SetTexture(UpdateSpectrumKernelIndex, "_InitialSpectrumTexture", initialSpectrumTexture);
+        fftComputeShader.SetTexture(UpdateSpectrumKernelIndex, "_UpdatedSpectrumTexture", updatedSpectrumTexture);
+        fftComputeShader.Dispatch(UpdateSpectrumKernelIndex, threadGroupsX, threadGroupsY, 1);
+
+        fftComputeShader.SetTexture(HeightMapKernelIndex, "_UpdatedSpectrumTexture", updatedSpectrumTexture);
+        fftComputeShader.SetTexture(HeightMapKernelIndex, "_HeightMap", heightMapTexture);
+        fftComputeShader.Dispatch(HeightMapKernelIndex, threadGroupsX, threadGroupsY, 1);
+
+        
+        Material debugMat = updatedSpectrumDebugQuad.GetComponent<Renderer>().material;
+        debugMat.SetTexture("_DebugTexture", updatedSpectrumTexture);
+
+        Material debugMat2 = heightMapDebugQuad.GetComponent<Renderer>().material;
+        debugMat2.SetTexture("_DebugTexture", heightMapTexture);
     }
 
     // Update is called once per frame
     void Update()
     {
-        fftComputeShader.SetFloat("_Wind_DirX", _Wind_DirX);
-        fftComputeShader.SetFloat("_Wind_DirY", _Wind_DirY);
-        fftComputeShader.SetFloat("_WindSpeed", _WindSpeed);
-        fftComputeShader.SetFloat("_A", _A);
-        fftComputeShader.SetInt("_Resolution", _Resolution);
-        fftComputeShader.SetInt("_PhysicalDomainLength", _PhysicalDomainLength);
-        fftComputeShader.SetTexture(0, "_InitialSpectrumTexture", initialSpectrumTexture);
-        fftComputeShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
-
-        Material debugMat = debugQuad.GetComponent<Renderer>().material;
-        debugMat.SetTexture("_DebugTexture", initialSpectrumTexture);
-
+        if (UpdateInitialSpectrum) {
+            UpdateInitalSpectrumTexture();
+        }
+        UpdateSpectrumTexture();
     }
 }
--- Assets/Shaders/Compute/HelperFunctions.hlsl
+++ Assets/Shaders/Compute/HelperFunctions.hlsl
@@ -1,6 +1,7 @@
 
 #define PI 3.14159265358979323846
 
+
 float PhillipsSpectrum(float A,float windSpeed,float2 windDirection, float2 waveVector)
 {
     float magnitudeOfWaveVector = length(waveVector);
@@ -36,4 +37,14 @@ float2 UniformToGaussian(float u1, float u2)
     float R = sqrt(-2.0 * log(u1));
     float theta = 2.0 * PI * u2;
     return float2(R * cos(theta), R * sin(theta));
+}
+
+float2 EulerFormula(float x) {
+    return float2(cos(x), sin(x));
+}
+// Helper function: complex multiplication of two complex numbers (a * b)
+// where a and b are represented as float2 (x = real, y = imaginary)
+float2 ComplexMultiplication(float2 a, float2 b)
+{
+    return float2(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);
 }
